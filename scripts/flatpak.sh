ccf5400c32336a8dd57f3a989652604555d800e9
coryc96 <corychambers96@gmail.com>
2022-06-20 16:17:41 -0400

    Create flatpak.sh

---
+++ scripts/flatpak.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -ex
+
+flatpkgs=(
+com.spotify.Client 
+org.ferdium.Ferdium
+org.telegram.desktop
+com.discordapp.Discord
+com.valvesoftware.Steam
+com.mattjakeman.ExtensionManager
+org.gnome.Extensions
+)
+
+flatpak install -y --noninteractive flathub "${flatpkgs[@]}"
