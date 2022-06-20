ac5e751af09e84805b65a37ce8754bbd849990e2
coryc96 <corychambers96@gmail.com>
2022-06-20 14:03:09 -0400

    Create launch_rdp.sh

---
+++ launch_rdp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export rdp_file="/home/${USER}/Downloads/Pub-RDPproxy*"
+#sed -i 's/audiocapturemode:i:0/audiocapturemode:i:1/' $file
+export file="/home/${USER}/.local/share/remmina/*myrdp-publix*"
+startup_path=$(awk -F ':' '/shell working directory/ {print $NF}' $rdp_file)
+load_balancer=$(awk -F ':' '/loadbalanceinfo/ {print $NF}' $rdp_file)
+sed -i "s/execpath=.*/execpath=${startup_path}/" $file
+sed -i "s/loadbalanceinfo=.*/loadbalanceinfo=${load_balancer}/" $file
+remmina -c $file
+#flatpak run org.remmina.Remmina -c $file
+rm $rdp_file
