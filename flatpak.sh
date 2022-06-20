409b28aa5bc16dd814635f085bafbb7e2adbfaf9
coryc96 <corychambers96@gmail.com>
2022-06-20 14:09:21 -0400

    Create flatpak.sh

---
+++ flatpak.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+flatpak install flathub com.spotify.Client
+flatpak install flathub org.ferdium.Ferdium
+flatpak install flathub org.telegram.desktop
+flatpak install flathub com.discordapp.Discord
+flatpak install flathub com.valvesoftware.Steam
+flatpak install flathub com.mattjakeman.ExtensionManager
+flatpak install flathub org.gnome.Extensions
