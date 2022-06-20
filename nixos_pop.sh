cee9009ad2e1d4b51daea25badeaf1569fe5050a
coryc96 <corychambers96@gmail.com>
2022-06-20 15:32:35 -0400

    Update nixos_pop.sh

--- nixos_pop.sh
+++ nixos_pop.sh
@@ -21,7 +21,7 @@ nix-env --install --attr "${NIXPKGS[@]}"
 
 flatpak remote-add --if-not-exists --user flathub https://flathub.org/repo/flathub.flatpakrepo
 
-flatpak install flatpak.sh
+sh flatpak.sh
 
 if [ ! -d "${HOME}/Projects/pop" ]
 then
