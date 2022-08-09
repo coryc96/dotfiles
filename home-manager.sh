ed829d86a5c8151f8e0602fd51c1c9097b6072f4
coryc96 <corychambers96@gmail.com>
2022-08-09 16:54:43 -0400

    Update home-manager.sh

--- home-manager.sh
+++ home-manager.sh
@@ -3,5 +3,7 @@
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 
+export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}
+
 # install HM
 nix-shell '<home-manager>' -A install
