53d976d627b90d8713287c2391593965a13df36b
coryc96 <corychambers96@gmail.com>
2023-03-03 17:18:42 -0500

    laptop fix

--- flake.nix
+++ flake.nix
@@ -1,10 +1,41 @@
 {
   description = "Personal Configuration Flake";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    hyprland.url = "github:hyprwm/Hyprland";
+
+    nixpkgs-wayland  = { url = "github:nix-community/nixpkgs-wayland"; };
+    
+    fu.url = "github:numtide/flake-utils";
+
+    eww = { 
+      url = "github:elkowar/eww";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.rust-overlay.follows = "rust-overlay";
+    };
+
+     rust-overlay = {
+       url = "github:oxalica/rust-overlay";
+       inputs.nixpkgs.follows = "nixpkgs";
+       inputs.flake-utils.follows = "fu";
+     };
+    # only needed if you use as a package set:
+    #nixpkgs-wayland.inputs.nixpkgs.follows = "cmpkgs";
+    #nixpkgs-wayland.inputs.master.follows = "master";
+  };
   
-  outputs = { self, nixpkgs, ... }@attrs: {
+  outputs = { self, nixpkgs, home-manager, hyprland, nixpkgs-wayland, fu, eww, rust-overlay, ... }@attrs: {
     nixosConfigurations.corypc = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+      let
+        system = "x86_64-linux";
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
       specialArgs = attrs;
       modules = [ 
         
@@ -48,6 +79,27 @@
       
         })
       ];
+    };
+    
+    homeConfigurations.coryc = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ 
+          ./home.nix
+          hyprland.homeManagerModules.default
+          ({pkgs, config, ...}: {
+            config = {
+          # use it as an overlay
+          nixpkgs.overlays = [ nixpkgs-wayland.overlay ];
+
+          home.packages = with pkgs; [
+            nixpkgs-wayland.packages.${system}.swww
+          ];
+
+        };
+      })];
     };
   };
 }
